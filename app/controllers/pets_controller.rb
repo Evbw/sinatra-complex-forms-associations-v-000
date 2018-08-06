1113279cf2e614882f78c3d1b60e8076476120f6
Everett <everett.bowline@gmail.com>
2018-08-06 00:13:49 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,10 +10,10 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-  @pet = Pet.create(params["pet"])
-    if !params["owner"]["name"].empty?
-     @pet.owner = Owner.create(name: params["owner"]["name"])
-   end
+    @pet = Pet.create(params["pet"])
+      if !params["owner"]["name"].empty?
+        @pet.owner = Owner.create(name: params["owner"]["name"])
+      end
    @pet.save
     redirect to "pets/#{@pet.id}"
   end
